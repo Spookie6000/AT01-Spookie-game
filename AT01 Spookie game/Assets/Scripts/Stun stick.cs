59aca002acaf9b33eaf0e5701fab3cb13a715dfa
Spookie6000 <125240888+Spookie6000@users.noreply.github.com>
2024-04-11 19:56:45 +0800

    FIX stun stick script

--- AT01 Spookie game/Assets/Scripts/Stun stick.cs
+++ AT01 Spookie game/Assets/Scripts/Stun stick.cs
@@ -5,24 +5,33 @@ using UnityEngine;
 public class Stunstick : MonoBehaviour
 {
     // set stun time 
-    public flaot stunDuration = 3.5f
+    public float stunDuration = 3.5f;
     private EnemyNPCMovement enemyNPCMovement;
 
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         // Find enemy movement script
-        enemyNPCMovement = GetComponent<enemyNPCMovement>();
+        enemyNPCMovement = GetComponent<EnemyNPCMovement>();
+        if (enemyNPCMovement == null )
 
     }
     // Activate StunState 
-    public void ActivatStun()
+    public void ActivateStun()
     {
         if (enemyNPCMovement != null) 
         {
             // calls the StunState form enemy script
             enemyNPCMovement.Stun(stunDuration);
+            Debug.log("Stunned");
+        }
+        else
+        {
+
+            Debug.logError("Stunned failled");
+
+
         }
 
 
