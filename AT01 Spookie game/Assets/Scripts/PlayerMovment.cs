e8189f3b51a1b2e5363e22d70d2f001847a80807
Spookie6000 <125240888+Spookie6000@users.noreply.github.com>
2024-04-26 09:56:41 +0800

    Fix Most movement errors
    12 errors fix
    3 new errors added

--- AT01 Spookie game/Assets/Scripts/PlayerMovment.cs
+++ AT01 Spookie game/Assets/Scripts/PlayerMovment.cs
@@ -3,19 +3,40 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+/// <summary>
+/// Chat GPT Has been used to modify this script
+/// </summary>
 
 public class PlayerMovment : MonoBehaviour
 {
     private CharacterController cTroller;
-
-    [SerializeField] private float speed = 3.0f;
+    // Sets the speed for the movement
+    [SerializeField] private float moveSpeed = 5.0f;
+    [SerializeField] private float spritSpeed = 10f;
+    [SerializeField] private float crouchSpeed = 2.5f;
+    // Srinting cooldown 
+    [SerializeField] private float sprintCooldown = 3f;
+    // Jump Hieght 
     [SerializeField] private float jumpHeight = 1.0f;
+    // Stamina Variables 
+    [SerializeField] private float maxStamina = 100;
+    [SerializeField] private float sprinStaminaDrainRate = 20f;
+    [SerializeField] private float sprintStaminaRecoveryRate = 10f;
+    private float currentStamina;
 
+
+    private bool isSpriting = false;
+    private bool isCrouchingWalking = false;
     private float gravityValue = -9.81f;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
 
-    Vector3 velocity;
+    // Sets the properties to get the sprinting and crouch 
+    public bool IsSprinting => isSprinting;
+    public bool IsCrouching => isCrouchingWalking;
+
+
+    private Vector3 velocity;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -37,26 +58,23 @@ public class PlayerMovment : MonoBehaviour
     void Start()
     {
 
-        if (Instance == null)
+        /*if (Instance == null)
         {
             Instance = this;
-        }
-
-
-
-
+        }*/
 
         gamePaused = false;
-        //cTroller = GetComponent<CharacterController>();
+        cTroller = GetComponent<CharacterController>();
+        currentStamina = maxStamina;
 
 
-        if (!TryGetComponent<CharacterController>(out cTroller))
-        {
-            Debug.Log("you need to attach a CharaterController to the player object you human");
+        /* if (!TryGetComponent<CharacterController>(out cTroller))
+         {
+             Debug.Log("you need to attach a CharaterController to the player object you human");
 
-            gameObject.SetActive(false);
+             gameObject.SetActive(false);
 
-        }
+         }*/
     }
 
     // Update is called once per frame
@@ -65,6 +83,13 @@ public class PlayerMovment : MonoBehaviour
         if (!gamePaused)
         {
             PlayerInput();
+        }
+        // Stamina Over Tinme
+        if (!isSpriting && currentStamina < maxStamina)
+        {
+            currentStamina += sprintStaminaRecoveryRate * Time.deletaTime;
+            currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+
         }
 
     }
@@ -91,23 +116,61 @@ public class PlayerMovment : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
 
+        float speed = IsSprinting ? spritSpeed : (isCrouchingWalking ? crouchSpeed : moveSpeed);
+
         cTroller.Move(move * speed * Time.deltaTime);
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityValue);
         }
-
+        // add garvity 
         velocity.y += gravityValue * Time.deltaTime;
         cTroller.Move(velocity * Time.deltaTime);
+        // Starts the spriting and checks the stamina 
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isSprinting && currentStamina > 0)
+        {
+            isSprinting = true;
+            Debug.Log("Out of Stamina");
+
+            //Stop sprintting
+            if (Input.GetKeyUp(KeyCode.LeftShift) || currentStamina <= 0)
+            {
+                isSprinting = false;
+            }
+            // Takes stamina while sprinting 
+            if (isSprinting)
+            {
+                currentStamina -= sprintStaminaConsumptionRate * Time.deltaTime;
+                // sets current stamina 
+                currentStamina = Mathf.Clamp(currentStamina, 0f, maxStamina);
+            }
 
 
+
+            // Crouch walking input
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                isCrouchWalking = true;
+            }
+            else if (Input.GetKeyUp(KeyCode.LeftControl))
+            {
+                isCrouchWalking = false;
+            }
+
+
+        }
+
+
+        private void OnDestroy()
+        {
+            EventManger.pauseGameEvent -= TogglePaused;
+
+
+        }
     }
-    private void OnDestroy()
-    {
-        EventManger.pauseGameEvent -= TogglePaused;
-    }
 
 
 
-}
+   
+
